5d575b48af7c78ac18763367bb9c542522ff441f
jo0132 <noji70770@gmail.com>
2023-03-31 03:14:29 +0900

    2023.03.31
    업로

--- homwork/js/solution-1.js
+++
@@ -1,9 +0,0 @@
-const imgMain = document.querySelector("#main > img");
-
-imgMain.addEventListener("mouseover", () => {
-    imgMain.src = "img/ara_02.jpg";
-});
-
-imgMain.addEventListener("mouseout", () => {
-    imgMain.src = "img/ara_01.jpg";
-});
