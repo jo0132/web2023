a3fcf31d0a8c373268c5cd85629beb9b4fedaf69
jo0132 <noji70770@gmail.com>
2023-04-25 20:35:28 +0900

    2023.04.25

---
+++ javascript/game/js/music.js
@@ -0,0 +1,53 @@
+const allMusic = [
+    {
+        name : "1.aaaa",
+        artist : "aaa",
+        img : "music_audio01",
+        audio : "1"
+    },{
+        name : "2.aaaa",
+        artist : "aaa",
+        img : "music_audio02",
+        audio : "2"
+    },{
+        name : "3.aaaa",
+        artist : "aaa",
+        img : "music_audio03",
+        audio : "3"
+    },{
+        name : "4.aaaa",
+        artist : "aaa",
+        img : "music_audio04",
+        audio : "4"
+    },{
+        name : "5.aaaa",
+        artist : "aaa",
+        img : "music_audio05",
+        audio : "5"
+    },{
+        name : "6.aaaa",
+        artist : "aaa",
+        img : "music_audio06",
+        audio : "6"
+    },{
+        name : "7.aaaa",
+        artist : "aaa",
+        img : "music_audio07",
+        audio : "7"
+    },{
+        name : "8.aaaa",
+        artist : "aaa",
+        img : "music_audio08",
+        audio : "8"
+    },{
+        name : "9.aaaa",
+        artist : "aaa",
+        img : "music_audio09",
+        audio : "9"
+    },{
+        name : "10.aaaa",
+        artist : "aaa",
+        img : "music_audio8",
+        audio : 8"
+    },
+]
