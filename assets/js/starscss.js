b547d1c77caa281ff06c511477d9384093bef665
jo0132 <noji70770@gmail.com>
2023-05-02 10:46:47 +0900

    2023.05.02

--- assets/js/starscss.js
+++ assets/js/starscss.js
@@ -1,114 +1,65 @@
-var canvas;
-var context;
-var screenH;
-var screenW;
-var stars = [];
-var fps = 50;
-var numStars = 2000;
+index16
+const stars = document.getElementById('stars');
+const starsCtx = stars.getContext('2d');
 
-$('document').ready(function() {
-  
-  // Calculate the screen size
-	screenH = $(window).height();
-	screenW = $(window).width();
-	
-	// Get the canvas
-	canvas = $('#space');
-	
-	// Fill out the canvas
-	canvas.attr('height', screenH);
-	canvas.attr('width', screenW);
-	context = canvas[0].getContext('2d');
-	
-	// Create all the stars
-	for(var i = 0; i < numStars; i++) {
-		var x = Math.round(Math.random() * screenW);
-		var y = Math.round(Math.random() * screenH);
-		var length = 1 + Math.random() * 2;
-		var opacity = Math.random();
-		
-		// Create a new star and draw
-		var star = new Star(x, y, length, opacity);
-		
-		// Add the the stars array
-		stars.push(star);
-	}
-	
-	setInterval(animate, 1000 / fps);
-});
+let screen, starsElements, starsParams = { speed: 4, number: 300, extinction: 4 };
 
-/**
- * Animate the canvas
- */
-function animate() {
-	context.clearRect(0, 0, screenW, screenH);
-	$.each(stars, function() {
-		this.draw(context);
-	})
+setupStars();
+updateStars();
+
+window.onresize = function () {
+    setupStars();
+};
+
+function Star() {
+    this.x = Math.random() * stars.width;
+    this.y = Math.random() * stars.height;
+    this.z = Math.random() * stars.width;
+
+    this.move = function () {
+        this.z -= starsParams.speed;
+        if (this.z 7<= 0) {
+            this.z = stars.width;
+        }
+    };
+
+    this.show = function () {
+        let x, y, rad, opacity;
+        x = (this.x - screen.c[0]) * (stars.width / this.z);
+        x = x + screen.c[0];
+        y = (this.y - screen.c[1]) * (stars.width / this.z);
+        y = y + screen.c[1];
+        rad = stars.width / this.z;
+        opacity = (rad > starsParams.extinction) ? 1.5 * (2 - rad / starsParams.extinction) : 1;
+
+        starsCtx.beginPath();
+        starsCtx.fillStyle = "rgba(255, 255, 255, " + opacity + ")";
+        starsCtx.arc(x, y, rad, 0, Math.PI * 2);
+        starsCtx.fill();
+    }
 }
 
-/**
- * Star
- * 
- * @param int x
- * @param int y
- * @param int length
- * @param opacity
- */
-function Star(x, y, length, opacity) {
-	this.x = parseInt(x);
-	this.y = parseInt(y);
-	this.length = parseInt(length);
-	this.opacity = opacity;
-	this.factor = 1;
-	this.increment = Math.random() * .03;
+function setupStars() {
+    screen = {
+        w: window.innerWidth,
+        h: window.innerHeight,
+        c: [window.innerWidth * 0.5, window.innerHeight * 0.5]
+    };
+    window.cancelAnimationFrame(updateStars);
+    stars.width = screen.w;
+    stars.height = screen.h;
+    starsElements = [];
+    for (let i = 0; i < starsParams.number; i++) {
+        starsElements[i] = new Star();
+    }
 }
 
-/**
- * Draw a star
- * 
- * This function draws a start.
- * You need to give the contaxt as a parameter 
- * 
- * @param context
- */
-Star.prototype.draw = function() {
-	context.rotate((Math.PI * 1 / 10));
-	
-	// Save the context
-	context.save();
-	
-	// move into the middle of the canvas, just to make room
-	context.translate(this.x, this.y);
-	
-	// Change the opacity
-	if(this.opacity > 1) {
-		this.factor = -1;
-	}
-	else if(this.opacity <= 0) {
-		this.factor = 1;
-		
-		this.x = Math.round(Math.random() * screenW);
-		this.y = Math.round(Math.random() * screenH);
-	}
-		
-	this.opacity += this.increment * this.factor;
-	
-	context.beginPath()
-	for (var i = 5; i--;) {
-		context.lineTo(0, this.length);
-		context.translate(0, this.length);
-		context.rotate((Math.PI * 2 / 10));
-		context.lineTo(0, - this.length);
-		context.translate(0, - this.length);
-		context.rotate(-(Math.PI * 6 / 10));
-	}
-	context.lineTo(0, this.length);
-	context.closePath();
-	context.fillStyle = "rgba(255, 255, 200, " + this.opacity + ")";
-	context.shadowBlur = 5;
-	context.shadowColor = '#ffff33';
-	context.fill();
-	
-	context.restore();
+function updateStars() {
+    starsCtx.fillStyle = "black";
+    starsCtx.fillRect(0, 0, stars.width, stars.height);
+    starsElements.forEach(function (s) {
+        s.show();
+        s.move();
+    });
+    window.requestAnimationFrame(updateStars);
 }
